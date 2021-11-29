1873f721bf8b26438a60d5fe16f49d386d4be65d
pizzmile <giacomo.pizza99@gmail.com>
2021-11-29 16:09:24 +0100

    enums

---
+++ src/main/java/it/polimi/telcodb2/TELCOEJB/enums/DurationEnum.java
@@ -0,0 +1,7 @@
+package it.polimi.telcodb2.TELCOEJB.enums;
+
+public enum DurationEnum {
+    TWELVE,
+    TWENTY_FOUR,
+    THIRTY_SIX;
+}
---
+++ src/main/java/it/polimi/telcodb2/TELCOEJB/enums/ServiceTypeEnum.java
@@ -0,0 +1,8 @@
+package it.polimi.telcodb2.TELCOEJB.enums;
+
+public enum ServiceTypeEnum {
+    FIXED_PHONE,
+    MOBILE_PHONE,
+    FIXED_INTERNET,
+    MOBILE_INTERNET;
+}
