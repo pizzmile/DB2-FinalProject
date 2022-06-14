af0e30627b262ac03c5023b24889fdbb486f9f9d
stefanoTaborelli <10608112@polimi.it>
2022-06-15 00:02:23 +0200

    First Servlet

---
+++ src/main/java/it/polimi/telcodb2/WEB/controllers/TemplateService.java
@@ -0,0 +1,42 @@
+package it.polimi.telcodb2.WEB.controllers;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/_Name_")
+public class TemplateService extends HttpServlet {
+
+    private TemplateEngine templateEngine;
+
+    private static final long serialVersionUID = 1L;
+
+    public TemplateService() {
+        super();
+    }
+
+    public void init() {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    }
+
+    public void destroy() {
+    }
+}
+
