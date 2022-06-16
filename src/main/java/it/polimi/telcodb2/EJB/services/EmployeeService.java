154a5225e40bd4120d2da2b3f7cbce957cf191fc
pizzmile <giacomo.pizza99@gmail.com>
2022-06-16 17:00:47 +0200

    removing corrupting code

--- src/main/java/it/polimi/telcodb2/EJB/services/EmployeeService.java
+++ src/main/java/it/polimi/telcodb2/EJB/services/EmployeeService.java
@@ -1,33 +1,33 @@
-package it.polimi.telcodb2.EJB.services;
-
-import it.polimi.telcodb2.EJB.entities.Employee;
-import it.polimi.telcodb2.EJB.exceptions.CredentialsException;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.NonUniqueResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
-import java.util.List;
-
-@Stateless
-public class EmployeeService {
-    // TODO: capire perchè non funzia @PersistenceContext(unitName = "EJB")
-    private EntityManager em;
-
-    public Employee checkCredentials(String username, String password) throws CredentialsException, NonUniqueResultException {
-        List<Employee> employeeList = null;
-        try {
-            employeeList = em.createNamedQuery("Employee.checkCredentials", Employee.class).setParameter(1, username).setParameter(2, password)
-                    .getResultList();
-        } catch (PersistenceException e) {
-            throw new CredentialsException("Could not verify credentials");
-        }
-        if (employeeList.isEmpty())
-            return null;
-        else if (employeeList.size() == 1)
-            return employeeList.get(0);
-        throw new NonUniqueResultException("More than one user registered with same credentials");
-
-    }
-}
+//package it.polimi.telcodb2.EJB.services;
+//
+//import it.polimi.telcodb2.EJB.entities.Employee;
+//import it.polimi.telcodb2.EJB.exceptions.CredentialsException;
+//
+//import javax.ejb.Stateless;
+//import javax.persistence.EntityManager;
+//import javax.persistence.NonUniqueResultException;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.PersistenceException;
+//import java.util.List;
+//
+//@Stateless
+//public class EmployeeService {
+//    // TODO: capire perchè non funzia @PersistenceContext(unitName = "EJB")
+//    private EntityManager em;
+//
+//    public Employee checkCredentials(String username, String password) throws CredentialsException, NonUniqueResultException {
+//        List<Employee> employeeList = null;
+//        try {
+//            employeeList = em.createNamedQuery("Employee.checkCredentials", Employee.class).setParameter(1, username).setParameter(2, password)
+//                    .getResultList();
+//        } catch (PersistenceException e) {
+//            throw new CredentialsException("Could not verify credentials");
+//        }
+//        if (employeeList.isEmpty())
+//            return null;
+//        else if (employeeList.size() == 1)
+//            return employeeList.get(0);
+//        throw new NonUniqueResultException("More than one user registered with same credentials");
+//
+//    }
+//}
