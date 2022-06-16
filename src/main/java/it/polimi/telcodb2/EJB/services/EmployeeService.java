6ed8021281d83a506553cd6a7c2f068c20c13d9a
pizzmile <giacomo.pizza99@gmail.com>
2022-06-17 00:24:00 +0200

    pointless changes

--- src/main/java/it/polimi/telcodb2/EJB/services/EmployeeService.java
+++ src/main/java/it/polimi/telcodb2/EJB/services/EmployeeService.java
@@ -17,16 +17,19 @@ public class EmployeeService {
 
     public Employee checkCredentials(String username, String password) throws CredentialsException, NonUniqueResultException {
         List<Employee> employeeList = null;
+
         try {
             employeeList = em.createNamedQuery("Employee.checkCredentials", Employee.class).setParameter(1, username).setParameter(2, password)
                     .getResultList();
         } catch (PersistenceException e) {
             throw new CredentialsException("Could not verify credentials");
         }
+
         if (employeeList.isEmpty())
             return null;
         else if (employeeList.size() == 1)
             return employeeList.get(0);
+
         throw new NonUniqueResultException("More than one user registered with same credentials");
 
     }
